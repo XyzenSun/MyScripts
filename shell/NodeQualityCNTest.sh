1cfe530befc3dbf123a941330d33130c0deab1b1
Xyzen <xyzen@duck.com>
2025-09-20 00:56:53 +0800

    Create NodeQualityCNTest.sh

---
+++ shell/NodeQualityCNTest.sh
@@ -0,0 +1,286 @@
+#!/bin/bash
+
+current_time="$(date +%Y_%m_%d_%H_%M_%S)"
+work_dir=".nodequality$current_time"
+# 在github下载链接前加入gh-proxy.com进行加速
+bench_os_url="https://gh-proxy.com/https://github.com/LloydAsp/NodeQuality/releases/download/v0.0.1/BenchOs.tar.gz"
+raw_file_prefix="https://gh-proxy.com/https://raw.githubusercontent.com/LloydAsp/NodeQuality/refs/heads/main"
+
+if uname -m | grep -Eq 'arm|aarch64'; then
+    # 在github下载链接前加入gh-proxy.com进行加速
+    bench_os_url="https://gh-proxy.com/https://github.com/LloydAsp/NodeQuality/releases/download/v0.0.1/BenchOs-arm.tar.gz"
+fi
+
+header_info_filename=header_info.log
+basic_info_filename=basic_info.log
+yabs_json_filename=yabs.json
+ip_quality_filename=ip_quality.log
+ip_quality_json_filename=ip_quality.json
+net_quality_filename=net_quality.log
+net_quality_json_filename=net_quality.json
+backroute_trace_filename=backroute_trace.log
+backroute_trace_json_filename=backroute_trace.json
+port_filename=port.log
+
+function start_ascii(){
+    echo -ne "\e[1;36m"
+    cat <<- EOF
+
+
+███╗   ██╗ ██████╗ ██████╗ ███████╗ ██████╗ ██╗   ██╗ █████╗ ██╗     ██╗████████╗██╗   ██╗
+████╗  ██║██╔═══██╗██╔══██╗██╔════╝██╔═══██╗██║   ██║██╔══██╗██║     ██║╚══██╔══╝╚██╗ ██╔╝
+██╔██╗ ██║██║   ██║██║  ██║█████╗  ██║   ██║██║   ██║███████║██║     ██║   ██║    ╚████╔╝ 
+██║╚██╗██║██║   ██║██║  ██║██╔══╝  ██║▄▄ ██║██║   ██║██╔══██║██║     ██║   ██║     ╚██╔╝  
+██║ ╚████║╚██████╔╝██████╔╝███████╗╚██████╔╝╚██████╔╝██║  ██║███████╗██║   ██║      ██║   
+╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝ ╚══▀▀═╝  ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝   ╚═╝      ╚═╝   
+                                                                                          
+                                                                             
+一个用于服务器的基准测试脚本，收集基本硬件信息、IP质量和网络质量
+
+基准测试将在临时系统中执行，之后所有痕迹都将被删除。
+因此，它对原始环境没有影响，并且支持几乎所有的Linux系统。
+
+作者：Lloyd@nodeseek.com
+Github：github.com/LloydAsp/NodeQuality
+命令：bash <(curl -sL https://run.NodeQuality.com)
+
+	EOF
+    echo -ne "\033[0m"
+}
+
+function _red() {
+    echo -e "\033[0;31m$1\033[0m"
+}
+
+function _yellow() {
+    echo -e "\033[0;33m$1\033[0m"
+}
+
+function _blue() {
+    echo -e "\033[0;36m$1\033[0m"
+}
+
+function _green() {
+    echo -e "\033[0;32m$1\033[0m"
+}
+
+function _red_bold() {
+    echo -e "\033[1;31m$1\033[0m"
+}
+
+function _yellow_bold() {
+    echo -e "\033[1;33m$1\033[0m"
+}
+
+function _blue_bold() {
+    echo -e "\033[1;36m$1\033[0m"
+}
+
+function _green_bold() {
+    echo -e "\033[1;32m$1\033[0m"
+}
+
+
+
+function pre_init(){
+    mkdir -p "$work_dir"
+    cd $work_dir
+    work_dir="$(pwd)"
+}
+
+function pre_cleanup(){
+    # incase interupted last time
+    clear_mount
+    if [[ "$work_dir" == *"nodequality"* ]]; then
+        rm -rf "${work_dir}"/*
+    else
+        echo "错误：工作目录不包含 'nodequality'！"
+        exit 1
+    fi
+}
+
+function clear_mount(){
+    swapoff $work_dir/swap 2>/dev/null
+
+    umount $work_dir/BenchOs/proc/ 2> /dev/null
+    umount $work_dir/BenchOs/sys/ 2> /dev/null
+    umount -R $work_dir/BenchOs/dev/ 2> /dev/null
+}
+
+function load_bench_os(){
+    cd $work_dir
+    rm -rf BenchOs
+
+    curl "-L#o" BenchOs.tar.gz $bench_os_url
+    tar -xzf BenchOs.tar.gz     
+    cd $work_dir/BenchOs
+
+    mount -t proc /proc proc/
+    mount --bind /sys sys/
+    mount --rbind /dev dev/
+    mount --make-rslave dev
+
+    rm etc/resolv.conf 2>/dev/null
+    cp /etc/resolv.conf etc/resolv.conf
+}
+
+function chroot_run(){
+    chroot $work_dir/BenchOs /bin/bash -c "$*"
+}
+
+function load_part(){
+    # gb5-test.sh, swap part
+    . <(curl -sL "$raw_file_prefix/part/swap.sh")
+}
+
+function load_3rd_program(){
+    # 在github下载链接前加入gh-proxy.com进行加速
+    chroot_run wget https://gh-proxy.com/https://github.com/nxtrace/NTrace-core/releases/download/v1.3.7/nexttrace_linux_amd64 -qO /usr/local/bin/nexttrace
+    chroot_run chmod u+x /usr/local/bin/nexttrace
+}
+
+function run_header(){
+    chroot_run bash <(curl -Ls "$raw_file_prefix/part/header.sh")
+}
+
+yabs_url="$raw_file_prefix/part/yabs.sh"
+function run_yabs(){
+    if ! curl -s 'https://browser.geekbench.com' --connect-timeout 5 >/dev/null; then
+        chroot_run bash <(curl -sL $yabs_url) -s -- -gi -w /result/$yabs_json_filename
+        echo -e "对 IPv6 单栈的服务器来说进行测试没有意义，\n因为要将结果上传到 browser.geekbench.com 后才能拿到最后的跑分，\n但 browser.geekbench.com 仅有 IPv4、不支持 IPv6，测了也是白测。"
+    else
+        virt=$(dmidecode -s system-product-name 2> /dev/null || virt-what | grep -v redhat | head -n 1 || echo "none")
+        if [[ "${virt,,}" != "lxc" ]]; then
+            check_swap 1>&2
+        fi
+        # 服务器一般测geekbench5即可
+        chroot_run bash <(curl -sL $yabs_url) -s -- -5i -w /result/$yabs_json_filename
+    fi
+
+    chroot_run bash <(curl -sL $raw_file_prefix/part/sysbench.sh)
+}
+
+function run_ip_quality(){
+    chroot_run bash <(curl -Ls IP.Check.Place) -n -o /result/$ip_quality_json_filename
+}
+
+function run_net_quality(){
+    local params=""
+    [[ "$run_net_quality_test" =~ ^[Ll]$ ]] && params=" -L"
+    chroot_run bash <(curl -Ls Net.Check.Place) $params -n -o /result/$net_quality_json_filename
+}
+
+function run_net_trace(){
+    chroot_run bash <(curl -Ls Net.Check.Place) -R -n -S 123 -o /result/$backroute_trace_json_filename
+}
+
+uploadAPI="https://api.nodequality.com/api/v1/record"
+function upload_result(){
+
+    chroot_run zip -j - "/result/*" > $work_dir/result.zip
+
+    base64 $work_dir/result.zip | curl -X POST  --data-binary @- $uploadAPI
+
+    echo
+}
+
+function post_cleanup(){
+    chroot_run umount -R /dev &> /dev/null
+    clear_mount
+
+    post_check_mount
+
+    rm -rf $work_dir/BenchOs
+
+    if [[ "$work_dir" == *"nodequality"* ]]; then
+        rm -rf "${work_dir}"/
+    else
+        echo "错误：工作目录不包含 'nodequality'！"
+        exit 1
+    fi
+
+    exit 1
+}
+
+function sig_cleanup(){
+    trap '' INT TERM SIGHUP EXIT
+    _red "正在清理，请稍候。"
+    post_cleanup
+}
+
+function post_check_mount(){
+    if mount | grep nodequality$current_time ; then
+        echo "出现了预料之外的情况，BenchOs目录的挂载未被清理干净，保险起见请重启后删除该目录" | tee $work_dir/error.log >&2
+        exit
+    fi
+}
+
+
+function ask_question(){
+    yellow='\033[1;33m'  # 设置黄色
+    reset='\033[0m'      # 重置颜色
+
+    echo -en "${yellow}是否运行基本信息测试？ (回车默认为 'y') [y/n]: ${reset}"
+    read run_yabs_test
+    run_yabs_test=${run_yabs_test:-y}
+
+    echo -en "${yellow}是否运行IP质量测试？ (回车默认为 'y') [y/n]: ${reset}"
+    read run_ip_quality_test
+    run_ip_quality_test=${run_ip_quality_test:-y}
+
+    echo -en "${yellow}是否运行网络质量测试？ (回车默认为 'y', 'l' 为低流量模式) [y/l/n]: ${reset}"
+    read run_net_quality_test
+    run_net_quality_test=${run_net_quality_test:-y}
+
+    echo -en "${yellow}是否运行回程路由追踪测试？ (回车默认为 'y') [y/n]: ${reset}"
+    read run_net_trace_test
+    run_net_trace_test=${run_net_trace_test:-y}
+}
+
+function main(){
+    trap 'sig_cleanup' INT TERM SIGHUP EXIT
+
+    start_ascii
+
+    ask_question
+
+    _green_bold '安装前清理'
+    pre_init
+    pre_cleanup
+    _green_bold '加载 BenchOs'
+    load_bench_os
+
+    load_part
+    load_3rd_program
+    _green_bold '基本信息'
+
+    result_directory=$work_dir/BenchOs/result
+    mkdir -p $result_directory
+    run_header > $result_directory/$header_info_filename
+
+    if [[ "$run_yabs_test" =~ ^[Yy]$ ]]; then
+        _green_bold '正在运行基本信息测试...'
+        run_yabs | tee $result_directory/$basic_info_filename
+    fi
+
+    if [[ "$run_ip_quality_test" =~ ^[Yy]$ ]]; then
+        _green_bold '正在运行IP质量测试...'
+        run_ip_quality | tee $result_directory/$ip_quality_filename
+    fi
+
+    if [[ "$run_net_quality_test" =~ ^[YyLl]$ ]]; then
+        _green_bold '正在运行网络质量测试...'
+        run_net_quality | tee $result_directory/$net_quality_filename
+    fi
+
+    if [[ "$run_net_trace_test" =~ ^[Yy]$ ]]; then
+        _green_bold '正在运行回程路由追踪...'
+        run_net_trace | tee $result_directory/$backroute_trace_filename
+    fi
+
+    upload_result
+    _green_bold '安装后清理'
+    post_cleanup
+}
+
+main
